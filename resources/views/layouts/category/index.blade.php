f4f22e924bccf495f7ae06a532f70c3ae0c36249
herry88 <herry081288@gmail.com>
2020-02-20 14:57:30 +0700

    XIRPL3

---
+++ resources/views/layouts/category/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+@section('title')
+    <title>Category Page</title>
+@endsection
+
+@section('content')
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+              <div class="row mb-2">
+                <div class="col-sm-6">
+                  <h1 class="m-0 text-dark">Category</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                  <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Home</li>
+                  </ol>
+                </div><!-- /.col -->
+              </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+          </div>
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="card">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
---
+++ resources/views/layouts/customer/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+    <title>Customer Page</title>
+@endsection
+
+@section('content')
+    <div class="content-wrapper">
+        <h1>Ini Halaman Customer</h1>
+    </div>
+@endsection
